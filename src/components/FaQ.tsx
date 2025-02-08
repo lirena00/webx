a442f13ebac9cc8b4d6d459729ef339c7ff4abb8
lirena <sakshamkushwaha12776@gmail.com>
2025-02-08 16:30:22 +0530

    stlyle: updated accordian

--- src/components/FaQ.tsx
+++ src/components/FaQ.tsx
@@ -3,35 +3,40 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "@radix-ui/react-accordion";
+} from "./ui/accordion";
 
 export function Faq() {
   return (
-    <div className="flex justify-center items-center">
-      <Accordion type="single" collapsible className="w-full max-w-3xl space-y-2 p-8 rounded-xl shadow-lg">
+    <div className="flex items-center justify-center">
+      <Accordion
+        type="single"
+        collapsible
+        className="w-full max-w-3xl space-y-2 rounded-xl p-8 shadow-lg"
+      >
         <AccordionItem value="item-1">
-          <AccordionTrigger className="text-[#ff6b6b] text-5xl font-bold">
+          <AccordionTrigger className="text-italic font-serif text-4xl font-bold text-[#ff6b6b]">
             Is it accessible?
           </AccordionTrigger>
-          <AccordionContent className="text-slate-100 text-3xl mt-4">
+          <AccordionContent className="mt-4 text-3xl text-slate-100">
             Yes. It adheres to the WAI-ARIA design pattern.
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-2">
-          <AccordionTrigger className="text-[#ff6b6b] text-5xl font-bold">
+          <AccordionTrigger className="text-5xl font-bold text-[#ff6b6b]">
             Is it styled?
           </AccordionTrigger>
-          <AccordionContent className="text-slate-100 text-3xl mt-4">
-            Yes. It comes with default styles that match the other components aesthetic.
+          <AccordionContent className="mt-4 text-3xl text-slate-100">
+            Yes. It comes with default styles that match the other components
+            aesthetic.
           </AccordionContent>
         </AccordionItem>
 
         <AccordionItem value="item-3">
-          <AccordionTrigger className="text-[#ff6b6b] text-5xl font-bold">
+          <AccordionTrigger className="text-5xl font-bold text-[#ff6b6b]">
             Is it animated?
           </AccordionTrigger>
-          <AccordionContent className="text-slate-100 text-3xl mt-4">
+          <AccordionContent className="mt-4 text-3xl text-slate-100">
             Yes. Its animated by default, but you can disable it if you prefer.
           </AccordionContent>
         </AccordionItem>
